cf45c6ad097709e5fb8669309d8b02e2c1c88a85
mia24sims <167013397+mia24sims@users.noreply.github.com>
2025-03-20 23:36:42 -0500

    Add animation and modified form container

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 function displayPoem(response) {
-  console.log(response.data);
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -17,6 +16,10 @@ function generatePoem(event) {
   let context =
     "You are highly skilled in haiku poems. Your assignment is to give a short and precise haiku poem with <br/> between the stanzas. Please make sure the poem is relating to the user's prompt. Sign the poem with `SheCodes AI`inside a HTML strong element at the end of the poem. ";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector(".poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = ` <div class = "blink">Generating haiku about ${instructionsInput.value}</div>`;
   axios.get(apiUrl).then(displayPoem);
 }
 
