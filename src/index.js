810ec08c27e72965d9da6afa1dfc1c5c871e29a3
mia24sims <167013397+mia24sims@users.noreply.github.com>
2025-03-20 21:56:23 -0500

    Added api integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
+  console.log(response.data);
   new Typewriter("#poem", {
-    strings: ["Generating Poem..."],
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "f2784deoc3ec5b8af02864390dt6ce37";
+  let prompt = `Please provide a short poem about ${instructionsInput.value}`;
+  let context =
+    "You are highly skilled in haiku poems. Your assignment is to give a short and precise haiku poem with <br/> between the stanzas. Please make sure the poem is relating to the user's prompt. Sign the poem with `SheCodes AI`inside a HTML strong element at the end of the poem. ";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemForm = document.querySelector("#poem-form");
