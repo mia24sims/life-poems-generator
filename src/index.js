216d9a6f32c0eb14ac4b24b8c1b81512582eb38e
mia24sims <167013397+mia24sims@users.noreply.github.com>
2025-03-20 19:40:37 -0500

    Added basic JS for form and typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("Hello World");
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: ["Generating Poem..."],
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemForm = document.querySelector("#poem-form");
+poemForm.addEventListener("submit", generatePoem);
